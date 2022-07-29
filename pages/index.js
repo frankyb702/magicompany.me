eb1e1fa848925e9912db25eec7a47d77e67dd70e
Frank B <frank@wiseapp.me>
2022-07-28 21:29:40 -0700

    Test Build Netlify
    Test Build

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Magi INC!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Test  <code>pages/index.js</code>
         </p>
       </main>
 
